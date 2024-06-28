ccc3b3838e2480a6fb8adf35c934f9af6966932f
PriankaMaheru190042112 <priankamaheru@iut-dhaka.edu>
2024-06-28 10:35:14 +0600

    Views and forms for Managing Projects and Tasks

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Project, Task
+from .forms import ProjectForm, TaskForm
 
-# Create your views here.
+@login_required
+def project_list(request):
+    projects = Project.objects.filter(members=request.user)
+    return render(request, 'projects/project_list.html', {'projects': projects})
+
+@login_required
+def project_detail(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    if request.user not in project.members.all():
+        return redirect('project_list')
+    tasks = project.tasks.all()
+    return render(request, 'projects/project_detail.html', {'project': project, 'tasks': tasks})
+
+@login_required
+def project_create(request):
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.save()
+            project.members.add(request.user)
+            return redirect('project_list')
+    else:
+        form = ProjectForm()
+    return render(request, 'projects/project_form.html', {'form': form})
+
+@login_required
+def task_create(request, project_pk):
+    project = get_object_or_404(Project, pk=project_pk)
+    if request.user not in project.members.all():
+        return redirect('project_list')
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.project = project
+            task.save()
+            return redirect('project_detail', pk=project_pk)
+    else:
+        form = TaskForm()
+    return render(request, 'projects/task_form.html', {'form': form, 'project': project})
+
+@login_required
+def task_update(request, project_pk, pk):
+    project = get_object_or_404(Project, pk=project_pk)
+    task = get_object_or_404(Task, pk=pk)
+    if request.user not in project.members.all():
+        return redirect('project_list')
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('project_detail', pk=project_pk)
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'projects/task_form.html', {'form': form, 'project': project, 'task': task})
