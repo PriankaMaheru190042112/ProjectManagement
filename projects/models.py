dc53813d515faeaf732823472ca8592c20e6cc99
PriankaMaheru190042112 <priankamaheru@iut-dhaka.edu>
2024-06-28 10:18:45 +0600

    Models for Projects, Tasks, and Users

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    members = models.ManyToManyField(User, related_name='projects')
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks', null=True, blank=True)
+    status = models.CharField(max_length=20, choices=[('Todo', 'Todo'), ('In Progress', 'In Progress'), ('Done', 'Done')], default='Todo')
+    due_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
